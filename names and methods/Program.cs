7e61d87402b96c0d788595fc5282a2378935071f
wambui7 <shiko.sheila@gmail.com>
2018-08-05 16:38:34 -0500

    Add project files.

---
+++ names and methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace names_and_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
