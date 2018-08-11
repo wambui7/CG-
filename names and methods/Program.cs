450ac3bf65b1528fe234139391de668de76cefc4
wambui7 <shiko.sheila@gmail.com>
2018-08-11 12:31:14 -0500

    no changes

--- names and methods/Program.cs
+++ names and methods/Program.cs
@@ -2,11 +2,51 @@
 
 namespace names_and_methods
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static (string FirstName, string LastName) BothNames { get; private set; }
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            // ask the user to enter first and last name
+        
+            Console.WriteLine("First name + Last name");
+
+            // ask the user whats his first name 
+            Console.Write("Whats your First Name");
+            string FirstName = Console.ReadLine();
+
+
+            // ask the user whats his last name 
+            Console.WriteLine("whats your Last Name");
+            string LastName = Console.ReadLine();
+
+            // combine both the firstname and the lastname
+             Console.Write(string.Format("{0} {1}", FirstName, LastName));
+
+            BothNames = (FirstName, LastName);
+
+
+            Console.ReadLine();
+
+
+        } 
+
 }
+
+    private static string BothNames(string FirstName, string LastName);
+
+            private string Fullnames = ("FirstName + LastName");
+
+       Return Fullname;
+   
+           
+
+    }
+
+
+       
+        
+
+       
+
